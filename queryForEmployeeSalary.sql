c3979e9e2bcfe490b250fa18843b517c60b26b48
Hafiz Kurniawan <99454377+hafizen@users.noreply.github.com>
2023-08-27 23:29:24 +0700

    query queryForEmployeeSalary.sql

---
+++ queryForEmployeeSalary.sql
@@ -0,0 +1,14 @@
+select *
+from employeesalary
+
+select distinct(jobtitle) as job
+from employeesalary
+
+select max(salary)
+from employeesalary
+
+select min(salary)
+from employeesalary
+
+select avg(salary)
+from employeesalary
