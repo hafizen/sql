384828ca3081f6be05b30ce3ada8225721596a34
Hafiz Kurniawan <99454377+hafizen@users.noreply.github.com>
2023-08-27 23:28:32 +0700

    query queryForEmployeeDemographics.sql

---
+++ queryForEmployeeDemographics.sql
@@ -0,0 +1,26 @@
+select *
+from employeeDemographics
+
+select firstname, lastname, age, gender
+from employeedemographics
+
+select firstname
+from employeedemographics
+
+select top 5 *
+from employeeDemographics
+
+select distinct(employeeID)
+from employeedemographics
+
+select distinct(gender)
+from employeedemographics
+
+select distinct(age)
+from employeedemographics
+
+select count(firstname)
+from employeedemographics
+
+select count(firstname) as 'total name'
+from employeedemographics
