4cb429a8122f8448ab282c3e1dcc9da3bc41e405
Hafiz Kurniawan <99454377+hafizen@users.noreply.github.com>
2023-08-27 23:25:32 +0700

    Create table.sql

---
+++ table.sql
@@ -0,0 +1,13 @@
+create table EmployeeDemographics(	
+  EmployeeID int,
+  FirstName varchar(50),
+  LastName varchar(50),
+  age int,
+  gender varchar(50)
+)
+
+create table EmployeeSalary(
+  EmployeeID int,
+  JobTitle varchar(50),
+  salary int
+)
