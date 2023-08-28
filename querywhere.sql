d236c662c975f4ef677fefd7a426fcfa450e1b7a
Hafiz Kurniawan <99454377+hafizen@users.noreply.github.com>
2023-08-28 23:09:51 +0700

    Create querywhere.sql

---
+++ querywhere.sql
@@ -0,0 +1,18 @@
+select *
+from EmployeeDemographics  
+
+select *
+from EmployeeDemographics
+where EmployeeID = 1001
+
+select *
+from EmployeeDemographics
+where EmployeeID <> 1001
+
+SELECT *
+from EmployeeDemographics
+where EmployeeId <>1001 and EmployeeID <> 1009
+
+select *
+from EmployeeDemographics
+WHERE firstname is not null
